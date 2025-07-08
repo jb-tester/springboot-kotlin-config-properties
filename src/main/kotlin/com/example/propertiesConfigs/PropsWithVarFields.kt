3e60e45aaa6b531d1c0bfb733f0ca2d39cc5f57b
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-07-08 16:25:33 +0200

    comments

--- src/main/kotlin/com/example/propertiesConfigs/PropsWithVarFields.kt
+++ src/main/kotlin/com/example/propertiesConfigs/PropsWithVarFields.kt
@@ -1,19 +1,28 @@
 package com.example.propertiesConfigs
 
 import com.example.data.AnotherBean
+import org.example.Enum1
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.NestedConfigurationProperty
 
 @ConfigurationProperties("vars.props")
 class PropsWithVarFields {
     /**
-     * vars.props.strProp prop doc.
+     * !!!vars.props.strProp prop doc.
      */
     var strProp: String? = "default"
     /**
-     * vars.props.boolProp prop doc.
+     * !!!vars.props.boolProp prop doc.
      */
     var boolProp: Boolean = false
+    /**
+     * !!!vars.props.enumProp prop doc.
+     */
+    var enumProp: Enum1 = Enum1.EL11
+    /**
+     * !!!vars.props.pojoMapProp prop doc.
+     */
+    var pojoMapProp: Map<String, AnotherBean> = mapOf()
 
    @NestedConfigurationProperty
     lateinit var anotherBean: AnotherBean
